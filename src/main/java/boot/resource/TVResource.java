55367fe80a3556138a74edb8091f1cebb7d6761a
Viggo Ludvigsen <subscription@viggo.info>
2015-01-15 19:30:01 +0100

    Adapt to Heroku

--- src/main/java/boot/resource/TVResource.java
+++ src/main/java/boot/resource/TVResource.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
 
@@ -17,13 +17,13 @@ public class TVResource {
     private TVService tvService;
 
     @RequestMapping("/schedule/{channel}")
-    public List<ScheduleDTO> getSchedule(@PathVariable String channel) {
+    public DeferredResult<List<ScheduleDTO>> getSchedule(@PathVariable String channel) {
         return tvService.getScheduledPrograms(channel);
     }
 
-    /**
-     * Add a RequestMapping and method to lookup your favorite show
-     */
-    // public ScheduleDTO getNextAiringOfTitle(...
+    @RequestMapping("/schedule/{channel}/next/{title}")
+    public DeferredResult<ScheduleDTO> getNextAiringOfTitle(@PathVariable String channel, @PathVariable String title) {
+         return tvService.nextOnAir(title);
+    }
 
 }
--- src/main/java/boot/service/TVService.java
+++ src/main/java/boot/service/TVService.java
@@ -1,8 +1,13 @@
 package boot.service;
 
 import boot.dto.ScheduleDTO;
+import boot.exception.NotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -11,16 +16,40 @@ import java.util.List;
 @Service
 public class TVService {
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private AsyncRestTemplate restTemplate = new AsyncRestTemplate();
 
-    public ScheduleDTO nextOnAir(String showTitle) {
-        // add logic to determine if the given show is on TV today
-        return null;
+    public DeferredResult<ScheduleDTO> nextOnAir(String showTitle) {
+
+        DeferredResult<ScheduleDTO> deferredResult = new DeferredResult<>();
+
+        final DeferredResult<List<ScheduleDTO>> scheduledPrograms = getScheduledPrograms("svt1");
+
+        scheduledPrograms.setResultHandler(svt1Programs -> {
+            ((List<ScheduleDTO>) svt1Programs).stream()
+                    .filter(program -> program.title.equalsIgnoreCase(showTitle))
+                    .findFirst()
+                    .ifPresent(deferredResult::setResult);
+            if(!scheduledPrograms.hasResult()) {
+                scheduledPrograms.setErrorResult(new NotFoundException(String.format("The program %s was not found", showTitle)));
+            }
+        });
+
+        return deferredResult;
+
     }
 
-    public List<ScheduleDTO> getScheduledPrograms(String channel) {
-        return Arrays.asList(restTemplate.getForObject("http://www.svt.se/play4api/channel/{channel}/schedule",
-                ScheduleDTO[].class, channel));
+    public DeferredResult<List<ScheduleDTO>> getScheduledPrograms(String channel) {
+
+        DeferredResult<List<ScheduleDTO>> deferredResult = new DeferredResult<>();
+        final ListenableFuture<ResponseEntity<ScheduleDTO[]>> listenableFuture = restTemplate.getForEntity("http://www.svt.se/play4api/channel/{channel}/schedule", ScheduleDTO[].class, channel);
+
+        listenableFuture.addCallback(result -> {
+            List<ScheduleDTO> list = Arrays.asList(result.getBody());
+            deferredResult.setResult(list);
+
+        }, deferredResult::setErrorResult);
+
+        return deferredResult;
     }
 
 }
